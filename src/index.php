bd938a247b2d75956b9c9eeb93f42605a0af2cbc
Chi Vo <c.kid2k11@gmail.com>
2021-05-29 10:51:39 +0700

    generate shorten

--- src/index.php
+++ src/index.php
@@ -1,11 +1,32 @@
 <?php
 
-namespace shorturl\HelloWorld;
+namespace ShortURL\ShortURL;
 
-class Index
+class Shorten
 {
-  public function greet($greet = "Hello World")
+  public static function create($url, $custom = "", $format = "json")
   {
-    return $greet;
+    $api_url = "https://short-url.asia/api/?key=FKuVTOWAyyXY";
+    $api_url .= "&url=" . urlencode(filter_var($url, FILTER_SANITIZE_URL));
+    if (!empty($custom)) {
+      $api_url .= "&custom=" . strip_tags($custom);
+    }
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_URL => $api_url
+    ));
+    $Response = curl_exec($curl);
+    curl_close($curl);
+    if ($format == "text") {
+      $Ar = json_decode($Response, TRUE);
+      if ($Ar["error"]) {
+        return $Ar["msg"];
+      } else {
+        return $Ar["short"];
+      }
+    } else {
+      return $Response;
+    }
   }
 }
